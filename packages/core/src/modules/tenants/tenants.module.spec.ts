3a77662abe07de04a2ef0e5779fbc7226d479dd4
adelfree2023-dev <adelfree2023@gmail.com>
2026-01-24 08:28:37 +0200

    test: resolve deep dependency resolution failures

--- packages/core/src/modules/tenants/tenants.module.spec.ts
+++ packages/core/src/modules/tenants/tenants.module.spec.ts
@@ -3,13 +3,17 @@ import { TenantsModule } from './tenants.module';
 import { TenantsService } from './tenants.service';
 import { PrismaModule } from '../../prisma/prisma.module';
 
+import { commonProviders } from '../../../test/test-utils';
+
 describe('TenantsModule', () => {
   let module: TestingModule;
 
   beforeAll(async () => {
     module = await Test.createTestingModule({
       imports: [TenantsModule],
-    }).compile();
+    })
+      .overrideProvider(PrismaModule).useValue({ providers: commonProviders })
+      .compile();
   });
 
   it('provides TenantsService', () => {
--- packages/core/src/prisma/prisma.module.spec.ts
+++ packages/core/src/prisma/prisma.module.spec.ts
@@ -2,11 +2,14 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { PrismaModule } from './prisma.module';
 import { PrismaService } from './prisma.service';
 
+import { commonProviders } from '../../test/test-utils';
+
 describe('PrismaModule', () => {
   let module: TestingModule;
   beforeAll(async () => {
     module = await Test.createTestingModule({
       imports: [PrismaModule],
+      providers: commonProviders,
     }).compile();
   });
 
